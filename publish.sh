42f3485ac8689af6f0519136377db4d8b219d19e
tianTian <tiantian20007@gmail.com>
2013-07-09 23:37:30 +0800

    do the clean job before build;

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
-./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c share -o ~/Desktop/ -t AnimationPro
+./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c share -o ~/Desktop/ -t AnimationPro -n
 ./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
 
-./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c Debug -o ~/Desktop/ -t DramaPlayer
+./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c share -o ~/Desktop/ -t AnimationPro91 -n
+./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
+
+./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c Debug -o ~/Desktop/ -t DramaPlayer -n
 ./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
