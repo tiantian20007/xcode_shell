520c618c9e4320d552732b322097cc22cb464ffa
tianTian <tiantian20007@gmail.com>
2013-07-09 19:01:05 +0800

    add publish.sh;

---
+++ publish.sh
@@ -0,0 +1,5 @@
+./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c share -o ~/Desktop/ -t AnimationPro
+./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
+
+./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c Debug -o ~/Desktop/ -t DramaPlayer
+./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
