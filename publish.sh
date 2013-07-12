8a547c1c77e64d3f07e80a8add943d9a2763c0a7
tianTian <tiantian20007@gmail.com>
2013-07-12 17:40:55 +0800

    add p parameter for product_name to find .app file;

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
-./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c share -o ~/Desktop/ -t AnimationPro -n
-./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
+./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t AnimationPro -n -p 烽火战神
+./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
 
-./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c share -o ~/Desktop/ -t AnimationPro91 -n
-./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
+#./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t AnimationPro91 -n -p 烽火战神91
+#./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
 
-./ipa-build /Users/leshu-2/Documents/repository/leshu/AnimationPro -c Debug -o ~/Desktop/ -t DramaPlayer -n
-./ipa-publish /Users/leshu-2/Documents/repository/leshu/AnimationPro
+#./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c Debug -o ~/Desktop/ -t DramaPlayer -n -p 剧本播放器
+#./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
