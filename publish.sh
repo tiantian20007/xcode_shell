ca9a91a7b5051ad6932fce86b81f68e26b8cbd3f
tianTian <tiantian20007@gmail.com>
2013-07-13 09:39:44 +0800

    more log;

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@
 #./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t AnimationPro91 -n -p 烽火战神91
 #./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
 
-#./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c Debug -o ~/Desktop/ -t DramaPlayer -n -p 剧本播放器
+#./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t DramaPlayer -n -p 剧本播放器
 #./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
