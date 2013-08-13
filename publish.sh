52657cd08fa12d9fcdc0629f6b035fa5a8b9a53a
tianTian <tiantian20007@gmail.com>
2013-08-13 20:20:31 +0800

    clean up;

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
-./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t AnimationPro -n -p 烽火战神
+./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c Debug -o ~/Desktop/ -t AnimationPro  -p 烽火战神
 ./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
 
-#./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t AnimationPro91 -n -p 烽火战神91
-#./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
+./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t AnimationPro91  -p 烽火战神91
+./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
 
-#./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t DramaPlayer -n -p 剧本播放器
-#./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
+./ipa-build /Users/leshu-2/Documents/repository/leshu/Fhzs-Release -c share -o ~/Desktop/ -t DramaPlayer  -p 剧本播放器
+./ipa-publish /Users/leshu-2/Documents/repository/leshu/Fhzs-Release
