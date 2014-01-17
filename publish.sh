7b5aba45496b1139910d0861dd52c3c24ad95917
iosTest <iosTest@gmail.com>
2014-01-17 21:38:27 +0800

    don't copy json file in data folder, copy unity3d instead; compile js file into jsc;

--- publish.sh
+++ publish.sh
@@ -2,6 +2,10 @@ XCODE_SHELL="/Users/leshu-2/Documents/repository/xcode_shell"
 SHARE_ROOT="/Users/leshu-2/Documents/repository/leshu/sjsg/shared"
 PUBLISH_IOS="/Users/leshu-2/Documents/repository/leshu/sjsg/published/IOS"
 
+JSTOJSC_PATH="/Users/leshu-2/Documents/repository/leshu/sjsg/libs/cocos2d-x/tools/cocos2d-console/console"
+publish_js="/Users/leshu-2/Documents/repository/leshu/sjsg/published/IOS/javascript/"
+lib_js="/Users/leshu-2/Documents/repository/leshu/sjsg/libs/cocos2d-x/scripting/javascript/bindings/js/"
+
 cd ${XCODE_SHELL}/
 
 # get publish date
@@ -38,10 +42,22 @@ echo start copy json file ...........................
 if [ -d "$PUBLISH_IOS"/data ]; then
     rm -rf "$PUBLISH_IOS"/data
 fi
+if [ -d "$PUBLISH_IOS"/data/ioris ]; then
+    rm -rf "$PUBLISH_IOS"/data/ioris
+fi
+if [ -d "$PUBLISH_IOS"/data/mais ]; then
+    rm -rf "$PUBLISH_IOS"/data/mais
+fi
+if [ -d "$PUBLISH_IOS"/data/monster ]; then
+    rm -rf "$PUBLISH_IOS"/data/monster
+fi
 
 mkdir "$PUBLISH_IOS"/data
+mkdir "$PUBLISH_IOS"/data/ioris
+mkdir "$PUBLISH_IOS"/data/mais
+mkdir "$PUBLISH_IOS"/data/monster
 
-for file in "$SHARE_ROOT"/res/data/*
+for file in "$SHARE_ROOT"/res/data/*.unity3d
     do
     if [ -d "$file" ]; then
         cp -rf "$file" "$PUBLISH_IOS"/data
@@ -51,6 +67,37 @@ for file in "$SHARE_ROOT"/res/data/*
         cp "$file" "$PUBLISH_IOS"/data
     fi
 done
+for file in "$SHARE_ROOT"/res/data/ioris/*.unity3d
+    do
+    if [ -d "$file" ]; then
+        cp -rf "$file" "$PUBLISH_IOS"/data/ioris
+    fi
+    
+    if [ -f "$file" ]; then
+        cp "$file" "$PUBLISH_IOS"/data/ioris
+    fi
+done
+for file in "$SHARE_ROOT"/res/data/mais/*.unity3d
+    do
+    if [ -d "$file" ]; then
+        cp -rf "$file" "$PUBLISH_IOS"/data/mais
+    fi
+    
+    if [ -f "$file" ]; then
+        cp "$file" "$PUBLISH_IOS"/data/mais
+    fi
+done
+for file in "$SHARE_ROOT"/res/data/monster/*.unity3d
+    do
+    if [ -d "$file" ]; then
+        cp -rf "$file" "$PUBLISH_IOS"/data/monster
+    fi
+    
+    if [ -f "$file" ]; then
+        cp "$file" "$PUBLISH_IOS"/data/monster
+    fi
+done
+
 
 # copy configlist file to publish dir
 for file in "$SHARE_ROOT"/res/config.plist
@@ -74,6 +121,15 @@ for file in "$XCODE_SHELL"/version.json
         cp "$file" "$PUBLISH_IOS"/version.json
     fi
 done
+
+# js to jsc
+python "$JSTOJSC_PATH"/cocos2d.py jscompile -s $publish_js -d $publish_js;
+echo clean js file in publish folders
+find $publish_js -type f -name "*.js" | xargs rm -rf;
+
+python "$JSTOJSC_PATH"/cocos2d.py jscompile -s $lib_js -d $lib_js;
+echo clean js file in lib folders
+find $lib_js -type f -name "*.js" | xargs rm -rf;
 
 #start publish ipa to php 
 echo start publish ipa...................................
